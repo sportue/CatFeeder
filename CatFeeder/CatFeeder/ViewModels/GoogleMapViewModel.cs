03f77a3450d138caa681eb45d223baab0e4c47f4
cihanoguzz <cihan.oguz@net-bt.com.tr>
2021-01-10 20:11:38 +0300

    gereksiz class silindi

--- CatFeeder/CatFeeder/ViewModels/GoogleMapViewModel.cs
+++ CatFeeder/CatFeeder/ViewModels/GoogleMapViewModel.cs
@@ -30,23 +30,5 @@ namespace CatFeeder.ViewModels
             return bowlLocation;
         }
 
-        internal async Task<System.Collections.Generic.List<Xamarin.Forms.GoogleMaps.Position>> LoadRoute()
-        {
-            var googleDirection = await ApiServices.ServiceClientInstance.GetDirections("28.127612", "82.309793", "28.163485", "82.314771");
-            if (googleDirection.Routes != null && googleDirection.Routes.Count > 0)
-            {
-                var positions = (Enumerable.ToList(PolylineHelper.Decode(googleDirection.Routes.First().OverviewPolyline.Points)));
-                return positions;
-            }
-            else
-            {
-                await App.Current.MainPage.DisplayAlert("Alert", "GOOGLE API ERROR", "Ok");
-                return null;
-
-            }
-
-        }
-
-
     }
 }
